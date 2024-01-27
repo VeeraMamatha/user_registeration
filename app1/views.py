404b76725a888d83da05b14db3c44a053c45f542
VeeraMamatha <muddamveeramamatha@gmail.com>
2024-01-27 12:14:00 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
+from django.http import HttpResponse,HttpResponseRedirect
 from app1.forms import *
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 
 
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            send_mail('registration',
+            'thanksfor regestration your regestrationis Successfull'
+            'mamathareddymuddam@gmail.com',
+            [MUFDO.email],
+            fail_silently=True)
+            return HttpResponse('Registartion Is Successfull')
+        else:
+            return HttpResponse('Invalid Data')
+
     return render(request,'registration.html',d)
+
+def home(request):
+    if request.session.get('username'):
+        username=request.sessiom.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def userlogin(request):
+    if request.method=='POST':
+        username=request.post['un']
+        password=request.post['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO) 
+            requestsession['username']=username
+            return HttpResponsRedirect(reverse('home'))
+    return render(request,'userlogin.html') 
+
+
--- project28/urls.py
+++ project28/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
-]
+    path('home/',home,name='home'),
+    path('userlogin',userlogin,name='userlogin'),
+
+    
+
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
